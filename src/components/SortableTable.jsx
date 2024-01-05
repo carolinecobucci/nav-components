77cb2e6a570902d7b6149e3506e47b58749dad67
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2024-01-05 09:58:10 -0300

    added sortable table component

---
+++ src/components/SortableTable.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+import Table from "./Table";
+
+const SortableTable = (props) => {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
+
+  const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
+
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+
+    return {
+      ...column,
+      header: () => (
+        <th className="cursor-pointer" onClick={() => handleClick(column.label)}>
+          <div className="flex items-center gap-1">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
+        </th>
+      ),
+    };
+  });
+
+  // Only sort data if sortOrder && sortBy are not null
+  // Make a copy of the 'data' prop
+  // Finf the correct sortValue function and use it for sorting
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
+};
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoTriangleUp /> <GoTriangleDown />
+      </div>
+    );
+  }
+
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoTriangleUp /> <GoTriangleDown />
+      </div>
+    );
+  } else if (sortOrder === "asc") {
+    return (
+      <div>
+        <GoTriangleUp />
+      </div>
+    );
+  } else if (sortOrder === "desc") {
+    return (
+      <div>
+        <GoTriangleDown />
+      </div>
+    );
+  }
+}
+
+export default SortableTable;
